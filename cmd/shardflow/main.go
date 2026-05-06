a80002e573bdf994bb84ae8278cdc136a22aacab
Het Patel <hetworkshard@gmail.com>
2026-05-06 17:41:53 +0530

    feat(shardflow): wire CLI root into client binary

--- cmd/shardflow/main.go
+++ cmd/shardflow/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/hett-patell/ShardFlow/internal/cli"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "shardflow: not yet implemented")
-	os.Exit(1)
+	if err := cli.NewRoot().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
