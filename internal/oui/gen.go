e845b4b395e33224f14b4a9e4141e5899218f2f6
Het Patel <hetworkshard@gmail.com>
2026-05-06 14:54:40 +0530

    fix(oui): correct awk dollar-sign escaping in go:generate directive

--- internal/oui/gen.go
+++ internal/oui/gen.go
@@ -3,4 +3,4 @@
 
 package oui
 
-//go:generate sh -c "curl -s https://standards-oui.ieee.org/oui/oui.txt | awk '/\\(base 16\\)/ { gsub(\"-\", \"\", $$1); print $$1, substr($$0, index($$0,\"(base 16)\")+11) }' > data/oui.txt"
+//go:generate sh -c "curl -s https://standards-oui.ieee.org/oui/oui.txt | awk '/\\(base 16\\)/ { gsub(\"-\", \"\", $1); print $1, substr($0, index($0,\"(base 16)\")+11) }' > data/oui.txt"
